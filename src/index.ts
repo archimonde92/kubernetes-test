67f504b40e68a42d5f9525552e22b1cccec3c92c
hoanld <hoanld@theparallel.io>
2025-06-23 10:50:38 +0700

    Update kubectl commands, change image tag to latest, increase replicas to 2, and update version to 1.1.3

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ app.get('/', (_req, res) => {
       <body>
         <h1>Color: ${COLOR}</h1>
         <p>Instance ID: ${instanceId}</p>
-        <p>Version: ${"1.1.2"}</p>
+        <p>Version: ${"1.1.3"}</p>
       </body>
     </html>
   `);
