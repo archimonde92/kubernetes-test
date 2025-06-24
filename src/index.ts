d2195004086a2e7f5d6bde918108708ed7038c92
hoanld <hoanld@theparallel.io>
2025-06-24 09:32:07 +0700

    Update version to 1.2.0 in index.ts

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ app.get('/', (_req, res) => {
       <body>
         <h1>Color: ${COLOR}</h1>
         <p>Instance ID: ${instanceId}</p>
-        <p>Version: ${"1.1.5"}</p>
+        <p>Version: ${"1.2.0"}</p>
       </body>
     </html>
   `);
